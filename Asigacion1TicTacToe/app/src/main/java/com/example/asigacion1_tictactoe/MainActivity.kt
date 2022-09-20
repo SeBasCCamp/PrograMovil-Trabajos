0b3fbbe386267f9cd778582e6e15a94d173cf6a5
fredy chávez campos <fschavezc@gmail.com>
2022-09-19 23:46:46 -0500

    actualizaciones

--- Asigacion1TicTacToe/app/src/main/java/com/example/asigacion1_tictactoe/MainActivity.kt
+++ Asigacion1TicTacToe/app/src/main/java/com/example/asigacion1_tictactoe/MainActivity.kt
@@ -9,18 +9,30 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     //Declarar jugadores y name para indicar a que jugador le toca
-    lateinit var noseleccion: IntArray
-    val jugador1 = 1
-    val jugador2 = 0
-    var actual = jugador1
+    lateinit var posTake: IntArray
+    val player1 = 1
+    val player2 = 0
+    var actual = player1
     lateinit var name: TextView
+
+    //Las combinaciones ganadoras
+    val positionWin:Array<IntArray> = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 9),
+        intArrayOf(1, 4, 7),
+        intArrayOf(2, 5, 8),
+        intArrayOf(3, 6, 9),
+        intArrayOf(1, 5, 9),
+        intArrayOf(7, 5, 3)
+    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        noseleccion = intArrayOf(
+        posTake = intArrayOf(
             -1,
             -1,
             -1,
@@ -61,52 +73,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         val btnClick = findViewById<Button>(p0!!.id) //Capturamos el id
         val presionaClick =
-            Integer.parseInt(btnClick.tag.toString()) //Cuando se presiona cualquier boton se almacena aca
+            Integer.parseInt(btnClick.tag.toString()) //Cuando se presiona cualquier boton se almacena aca el tag
 
 
-        if (noseleccion[presionaClick] != -1)
+        if (posTake[presionaClick] != -1)
             return
 
-        noseleccion[presionaClick] = actual
+        posTake[presionaClick] = actual
 
-        if (actual == jugador1) {
+        if (actual == player1) {
             btnClick.text = "O"
-            actual = jugador2
+            actual = player2
             name.text = "Le toca al jugador 2"
         } else {
             btnClick.text = "X"
-            actual = jugador1
+            actual = player1
             name.text = "Le toca al jugador 1"
         }
-        win()
+
 
     }
 
-    private fun win() {
-        var positionWin = arrayOf(
-            intArrayOf(1, 2, 3),
-            intArrayOf(4, 5, 6),
-            intArrayOf(7, 8, 9),
-            intArrayOf(1, 4, 7),
-            intArrayOf(2, 5, 8),
-            intArrayOf(3, 6, 9),
-            intArrayOf(1, 5, 9),
-            intArrayOf(7, 5, 3),
-            intArrayOf(3, 5, 7)
-        )
-        for (i in 0 until positionWin.size){
-            var primer = positionWin[i][0]
-            var segundo = positionWin[i][1]
-            var tercero = positionWin[i][2]
 
-            if(noseleccion[primer]==noseleccion[segundo] && noseleccion[segundo]==noseleccion[tercero]){
-                if(noseleccion[primer]==jugador1){
-                    name.text = "El jugador 1 es el ganador"
-                }else{
-                    name.text="El jugador 2 es el ganador"
-                }
-            }
-        }
-    }
 }
 
