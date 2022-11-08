600e13d48552f710517866451d7e4252ee97a12d
fredy chávez campos <fschavezc@gmail.com>
2022-11-07 23:00:09 -0500

    a

--- Asignacion3/app/src/main/java/com/example/asignacion3/Pantallas/data/source/remote/EquiposAPI.kt
+++ Asignacion3/app/src/main/java/com/example/asignacion3/Pantallas/data/source/remote/EquiposAPI.kt
@@ -4,7 +4,7 @@ import com.example.asignacion3.Pantallas.data.source.remote.dto.TeamDto
 import com.example.asignacion3.Pantallas.data.source.remote.dto.TeamsDto
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
+
 
 interface EquiposAPI {
     @GET("competitions/WC/teams/{id}")
